cac379a7a5aa436d4f125ca4a25a3b9fdd9f5b69
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 01:38:51 +0700

    Update 76.c

--- 76.c
+++ 76.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    FILE *file = fopen("output.txt", "r");
-    if (file != NULL) {
-        fseek(file, 5, SEEK_SET); // Смещение на 5 байт от начала
-        char ch = fgetc(file);
-        printf("6-й символ в файле: %c\n", ch);
-        fclose(file);
+    int num;
+    printf("Введите число: ");
+    scanf("%d", &num);
+    if (num % 2 == 0) {
+        printf("Чётное\n");
+    } else {
+        printf("Нечётное\n");
     }
     return 0;
 }
