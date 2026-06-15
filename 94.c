48a33e8767dc88e99980b506c13662501d7577f8
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 00:12:19 +0700

    Create 94.c

---
+++ 94.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void removeSpaces(char *str) {
+    int i = 0, j = 0;
+    while (str[i]) {
+        if (str[i] != ' ') {
+            str[j++] = str[i];
+        }
+        i++;
+    }
+    str[j] = '\0';
+}
+
+int main() {
+    char str[] = "a b c d e";
+    removeSpaces(str);
+    printf("Без пробелов: %s\n", str);
+    return 0;
+}
