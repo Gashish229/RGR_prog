1b5c4845c3230770edfb5e4359f784063505e639
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 01:41:50 +0700

    Update 81.c

--- 81.c
+++ 81.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 
-void executeCallback(void (*callback)()) {
-    printf("Подготовка...\n");
-    callback();
-}
-
-void myTask() {
-    printf("Выполнение задачи!\n");
-}
-
 int main() {
-    executeCallback(myTask);
+    int month;
+    printf("Введите номер месяца: ");
+    scanf("%d", &month);
+    switch(month) {
+        case 1: printf("Январь\n"); break;
+        case 2: printf("Февраль\n"); break;
+        case 3: printf("Март\n"); break;
+        case 4: printf("Апрель\n"); break;
+        case 5: printf("Май\n"); break;
+        case 6: printf("Июнь\n"); break;
+        case 7: printf("Июль\n"); break;
+        case 8: printf("Август\n"); break;
+        case 9: printf("Сентябрь\n"); break;
+        case 10: printf("Октябрь\n"); break;
+        case 11: printf("Ноябрь\n"); break;
+        case 12: printf("Декабрь\n"); break;
+        default: printf("Неверный номер месяца\n");
+    }
     return 0;
 }
