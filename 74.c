badc73c1303143872452e40935764042fc4b8fa9
Gashish229 <SINKOV07@BK.RU>
2026-06-15 23:57:24 +0700

    Create 74.c

---
+++ 74.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int choice = 2;
+
+    if (choice == 1) {
+        printf("Action 1\n");
+    } else if (choice == 2) {
+        printf("Action 2\n");
+    } else if (choice == 3) {
+        printf("Action 3\n");
+    }
+    return 0;
+}
