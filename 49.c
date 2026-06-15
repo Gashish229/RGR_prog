8f8faa7ecc6f4f65e629b08a6b92e8857670b291
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 01:36:50 +0700

    Update 49.c

--- 49.c
+++ 49.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
+void replace_spaces(char *str, char replacement) {
+    for(int i = 0; str[i] != '\0'; i++) {
+        if(str[i] == ' ') {
+            str[i] = replacement;
+        }
+    }
+}
+
 int main() {
-    int a = 42;
-    int *p = &a;
-    int **pp = &p;
-    
-    printf("Значение a через двойной указатель: %d\n", **pp);
+    char str[] = "текст с пробелами";
+    replace_spaces(str, '_');
+    printf("Результат: %s\n", str);
     return 0;
 }
