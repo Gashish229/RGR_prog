2c3c64be2e21d52a4ac3b00e4cec821b6284d5f8
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 01:55:30 +0700

    Update 97.c

--- 97.c
+++ 97.c
@@ -1,45 +1,14 @@
 #include <stdio.h>
 
-void reverse(char str[], int length) {
-    int start = 0;
-    int end = length - 1;
-    while (start < end) {
-        char temp = str[start];
-        str[start++] = str[end];
-        str[end--] = temp;
-    }
-}
-
-char* myItoa(int num, char* str, int base) {
-    int i = 0;
-    int isNegative = 0;
-    
-    if (num == 0) {
-        str[i++] = '0';
-        str[i] = '\0';
-        return str;
-    }
-    
-    if (num < 0 && base == 10) {
-        isNegative = 1;
-        num = -num;
-    }
-    
-    while (num != 0) {
-        int rem = num % base;
-        str[i++] = (rem > 9) ? (rem - 10) + 'a' : rem + '0';
-        num = num / base;
-    }
-    
-    if (isNegative) str[i++] = '-';
-    str[i] = '\0';
-    reverse(str, i);
-    return str;
-}
-
 int main() {
-    char buffer[20];
-    myItoa(156, buffer, 10);
-    printf("Строка: %s\n", buffer);
+    int a, b, gcd = 1;
+    printf("Введите два числа: ");
+    scanf("%d %d", &a, &b);
+    for (int i = 1; i <= a && i <= b; i++) {
+        if (a % i == 0 && b % i == 0) {
+            gcd = i;
+        }
+    }
+    printf("НОД: %d\n", gcd);
     return 0;
 }
