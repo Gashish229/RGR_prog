a762e0994fe679cee29b7e8de0ebed95c71a0f50
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 01:35:45 +0700

    Update 46.c

--- 46.c
+++ 46.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int a[2][2] = {{1, 2}, {3, 4}};
-    int b[2][2] = {{5, 6}, {7, 8}};
-    int sum[2][2];
+    char str[] = "  a b   c d  ";
+    int j = 0;
     
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
-            sum[i][j] = a[i][j] + b[i][j];
-            printf("%d ", sum[i][j]);
+    for(int i = 0; str[i] != '\0'; i++) {
+        if(str[i] != ' ') {
+            str[j++] = str[i];
         }
-        printf("\n");
     }
+    str[j] = '\0';
+    printf("Без пробелов: '%s'\n", str);
     return 0;
 }
