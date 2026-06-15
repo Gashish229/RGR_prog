ed1dc9f22627dc1b8f5786200d4981fcb81a0c97
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 01:56:40 +0700

    Update 99.c

--- 99.c
+++ 99.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-struct Node {
-    int data;
-    struct Node* next;
-};
-
-void push(struct Node** head_ref, int new_data) {
-    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
-    new_node->data = new_data;
-    new_node->next = (*head_ref);
-    (*head_ref) = new_node;
-}
 
 int main() {
-    struct Node* head = NULL;
-    push(&head, 10);
-    printf("Элемент добавлен.\n");
+    int n, sum = 0;
+    printf("Введите число: ");
+    scanf("%d", &n);
+    for (int i = 1; i < n; i++) {
+        if (n % i == 0) sum += i;
+    }
+    if (sum == n && n != 0) {
+        printf("Число совершенное\n");
+    } else {
+        printf("Число не совершенное\n");
+    }
     return 0;
 }
