e93b614f04df8a1570c04be8530ad2a2f501544f
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 01:43:10 +0700

    Update 84.c

--- 84.c
+++ 84.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    int a = 5;  // 0101
-    int b = 3;  // 0011
-    
-    printf("Побитовое И (a & b): %d\n", a & b); // 0001 (1)
-    printf("Побитовое ИЛИ (a | b): %d\n", a | b); // 0111 (7)
-    printf("Поиск искл. ИЛИ (a ^ b): %d\n", a ^ b); // 0110 (6)
+    int day;
+    printf("Введите день недели (1-7): ");
+    scanf("%d", &day);
+    switch(day) {
+        case 1: case 2: case 3: case 4: case 5:
+            printf("Рабочий день\n");
+            break;
+        case 6: case 7:
+            printf("Выходной день\n");
+            break;
+        default:
+            printf("Неверный день недели\n");
+    }
     return 0;
 }
