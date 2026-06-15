f37aba2ff543ff7b3d47e7260c440cb0d4aa1952
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 02:06:35 +0700

    Update 130.c

--- 130.c
+++ 130.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-struct Data { int id; float val; };
-
 int main() {
-    struct Data d;
-    FILE *file = fopen("struct.bin", "rb");
-    if (file != NULL) {
-        fread(&d, sizeof(struct Data), 1, file);
-        printf("Прочитано: id=%d, val=%.1f\n", d.id, d.val);
-        fclose(file);
+    FILE *f = fopen("text.txt", "r");
+    if (!f) return 1;
+    
+    char line[256];
+    while (fgets(line, sizeof(line), f)) {
+        printf("%s", line);
     }
+    
+    fclose(f);
     return 0;
 }
