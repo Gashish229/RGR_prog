50ed3b283e29e8b59b91dc2666a296081f7781a4
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 01:34:59 +0700

    Update 44.c

--- 44.c
+++ 44.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    int arr[] = {1, 2, 3, 4, 5};
-    int n = 5;
-    
-    for (int i = 0; i < n / 2; i++) {
-        int temp = arr[i];
-        arr[i] = arr[n - 1 - i];
-        arr[n - 1 - i] = temp;
+    int c;
+    printf("Введите текст (Enter для выхода):\n");
+    while((c = getchar()) != '\n' && c != EOF) {
+        putchar(c);
     }
-    
-    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+    printf("\n");
     return 0;
 }
