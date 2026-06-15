16b553669906bda2c63f4f08400da347e435b26c
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 01:46:52 +0700

    Update 91.c

--- 91.c
+++ 91.c
@@ -1,24 +1,9 @@
 #include <stdio.h>
-#include <ctype.h>
-
-int countWords(const char *str) {
-    int count = 0;
-    int inWord = 0;
-
-    while (*str) {
-        if (isspace(*str)) {
-            inWord = 0;
-        } else if (!inWord) {
-            inWord = 1;
-            count++;
-        }
-        str++;
-    }
-    return count;
-}
 
 int main() {
-    char text[] = "Привет, как твои дела?";
-    printf("Количество слов: %d\n", countWords(text));
+    for (int i = 1; i <= 10; i++) {
+        printf("%d ", i * i);
+    }
+    printf("\n");
     return 0;
 }
