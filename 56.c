12f9900313d1c4448ec5b98ccf1d550b42b750e8
Gashish229 <SINKOV07@BK.RU>
2026-06-15 23:48:35 +0700

    Create 56.c

---
+++ 56.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int num = 7;
+    if (num >= 1 && num <= 10) {
+        printf("В диапазоне\n");
+    } else {
+        printf("Вне диапазона\n");
+    }
+    return 0;
+}
