72cc62b554162f091743509abbf4ea590f65e678
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 01:30:29 +0700

    Update 33.c

--- 33.c
+++ 33.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-    int i = 1;
-    while (i <= 5) {
-        printf("%d ", i);
-        i++;
+    float a = 0.1f * 3.0f;
+    float b = 0.3f;
+    float epsilon = 1e-5f; // Погрешность
+
+    if (fabs(a - b) < epsilon) {
+        printf("Числа равны с учетом погрешности\n");
+    } else {
+        printf("Числа не равны\n");
     }
     return 0;
 }
