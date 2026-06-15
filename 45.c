e56844b0e33634f064c14c3af9f8b707c41422d2
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 00:01:34 +0700

    Create 45.c

---
+++ 45.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int matrix[2][2] = {
+        {1, 2},
+        {3, 4}
+    };
+    printf("Элемент [1][0]: %d\n", matrix[1][0]); // Выведет 3
+    return 0;
+}
