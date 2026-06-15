6d512b1a55b28ecff91cf03eae4950df8bc2b6e3
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-15 23:49:22 +0700

    Create 27.c

---
+++ 27.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5, b = 5, c = 2, d = 4;
+    int result = (a + b) * c / d; // (10) * 2 / 4 = 5
+    printf("Результат: %d\n", result);
+    return 0;
+}
