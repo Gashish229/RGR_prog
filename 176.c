3be5b97a563e8e282c22dbf662133ea56c8aae44
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 00:43:27 +0700

    Create 176.c

---
+++ 176.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char str[] = "programming";
+    int freq[256] = {0};
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        freq[(unsigned char)str[i]]++;
+    }
+
+    for (int i = 0; i < 256; i++) {
+        if (freq[i] > 0) printf("'%c' : %d\n", i, freq[i]);
+    }
+    return 0;
+}
