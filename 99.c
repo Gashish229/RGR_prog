46e4de4a1df798b04b9ceb1590a46d3ef00baa13
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 00:14:07 +0700

    Create 99.c

---
+++ 99.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+void push(struct Node** head_ref, int new_data) {
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+    new_node->data = new_data;
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+
+int main() {
+    struct Node* head = NULL;
+    push(&head, 10);
+    printf("Элемент добавлен.\n");
+    return 0;
+}
