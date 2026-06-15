674c0aa64942e840e8a5260a3f0ed6f1289a7955
Gashish229 <SINKOV07@BK.RU>
2026-06-16 00:04:33 +0700

    Create 102.c

---
+++ 102.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int i = 1;
+    while (i <= 20) {
+        printf("%d ", i);
+        i++; // Инкремент счетчика
+    }
+    printf("\n");
+    return 0;
+}
