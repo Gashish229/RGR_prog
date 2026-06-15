51cbd362a7ce81eed45c158532c8e2f295ce1137
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 01:39:47 +0700

    Update 78.c

--- 78.c
+++ 78.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int main() {
-    FILE *file = fopen("output.txt", "r");
-    if (file != NULL) {
-        int num;
-        // feof возвращает истину только ПОСЛЕ неудачной попытки чтения за концом файла
-        while (fscanf(file, "%d", &num) == 1) { 
-            printf("Число: %d\n", num);
-        }
-        if (feof(file)) printf("Достигнут конец файла.\n");
-        fclose(file);
+    char c;
+    printf("Введите символ: ");
+    scanf("%c", &c);
+    if (isalpha(c)) {
+        printf("Это буква\n");
+    } else if (isdigit(c)) {
+        printf("Это цифра\n");
+    } else {
+        printf("Это специальный символ\n");
     }
     return 0;
 }
