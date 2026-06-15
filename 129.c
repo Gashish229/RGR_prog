0d082756a7304074054662dcdc9b116ed13aace2
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 02:06:08 +0700

    Update 129.c

--- 129.c
+++ 129.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
-struct Data { int id; float val; };
+struct Person {
+    char name[50];
+    int age;
+};
 
 int main() {
-    struct Data d1 = {1, 99.5f};
-    FILE *file = fopen("struct.bin", "wb");
-    if (file != NULL) {
-        fwrite(&d1, sizeof(struct Data), 1, file);
-        fclose(file);
-        printf("Структура записана.\n");
+    struct Person p1 = {"Ivan", 30}, p2;
+    
+    FILE *out = fopen("person.bin", "wb");
+    if (out) {
+        fwrite(&p1, sizeof(struct Person), 1, out);
+        fclose(out);
+    }
+    
+    FILE *in = fopen("person.bin", "rb");
+    if (in) {
+        fread(&p2, sizeof(struct Person), 1, in);
+        printf("Загружено: %s, %d лет\n", p2.name, p2.age);
+        fclose(in);
     }
     return 0;
 }
