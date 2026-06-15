1b21076df717a712975554717cd95cd16fe70401
Gashish229 <SINKOV07@BK.RU>
2026-06-15 23:47:39 +0700

    Create 54.c

---
+++ 54.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int num = 42;
+    if (num % 2 == 0) {
+        printf("Четное\n");
+    } else {
+        printf("Нечетное\n");
+    }
+    return 0;
+}
