8ddf6ed96aabe656e3d379b0c54f1e1212cd328c
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 01:34:05 +0700

    Update 42.c

--- 42.c
+++ 42.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int arr[] = {7, 1, 4, 0, 9};
-    int min = arr[0];
-    
-    for (int i = 1; i < 5; i++) {
-        if (arr[i] < min) {
-            min = arr[i];
-        }
+    char a = 'A';
+    char b = 'Z';
+    if (a < b) {
+        printf("'%c' стоит в алфавите перед '%c'\n", a, b);
     }
-    printf("Минимум: %d\n", min);
     return 0;
 }
