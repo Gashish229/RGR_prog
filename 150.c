0b6bd610cec495693a8aa07218a43caafdc0e548
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 00:27:34 +0700

    Create 150.c

---
+++ 150.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    if (isPrime(17)) printf("17 - простое число\n");
+    return 0;
+}
