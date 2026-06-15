184d3543f67079e335a9f30e8df119a864cd4993
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 01:36:08 +0700

    Update 47.c

--- 47.c
+++ 47.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int main() {
-    int a = 10;
-    int *p = &a;
-    printf("Адрес переменной a: %p\n", (void*)p);
+    char str[] = "Это   пример  строки с несколькими словами";
+    int count = 0, in_word = 0;
+
+    for(int i = 0; str[i] != '\0'; i++) {
+        if(!isspace(str[i]) && !in_word) {
+            in_word = 1;
+            count++;
+        } else if(isspace(str[i])) {
+            in_word = 0;
+        }
+    }
+    printf("Количество слов: %d\n", count);
     return 0;
 }
