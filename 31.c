64ce6201358ad77915fa17bf600bdf71b9242db1
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-15 23:51:43 +0700

    Add program to check if a number is even or odd

---
+++ 31.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int num = 4;
+    if (num % 2 == 0) {
+        printf("Число четное\n");
+    } else {
+        printf("Число нечетное\n");
+    }
+    return 0;
+}
