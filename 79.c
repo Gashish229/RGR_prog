e754217cdb44e4d95ef77fc31b228bfda8963ba2
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 01:40:35 +0700

    Update 79.c

--- 79.c
+++ 79.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
-void sayHello() {
-    printf("Привет от указателя на функцию!\n");
-}
-
 int main() {
-    void (*funcPtr)() = sayHello; // Объявление указателя
-    funcPtr(); // Вызов функции через указатель
+    int mark;
+    printf("Введите оценку (1-5): ");
+    scanf("%d", &mark);
+    switch(mark) {
+        case 1: printf("Плохо\n"); break;
+        case 2: printf("Неудовлетворительно\n"); break;
+        case 3: printf("Удовлетворительно\n"); break;
+        case 4: printf("Хорошо\n"); break;
+        case 5: printf("Отлично\n"); break;
+        default: printf("Неверная оценка\n");
+    }
     return 0;
 }
