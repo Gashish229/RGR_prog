117528fdeff17a3d15b89922acf514c6f91d078e
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 07:17:53 +0700

    Update 137.c

--- 137.c
+++ 137.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 int main() {
-    int i = 0;
-    printf("Постфикс (i++ < 3):\n");
-    while (i++ < 3) printf("%d ", i); // Выведет 1 2 3
-    
-    printf("\nПрефикс (++j < 3):\n");
-    int j = 0;
-    while (++j < 3) printf("%d ", j); // Выведет 1 2
-    printf("\n");
+    int x = 10, y = 20;
+    swap(&x, &y);
+    printf("x = %d, y = %d\n", x, y);
     return 0;
 }
