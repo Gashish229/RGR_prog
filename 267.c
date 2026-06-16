1d6fd19cf23939b57ca875ae56736ceb1a8f031d
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 08:05:41 +0700

    Update 267.c

--- 267.c
+++ 267.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
-int main() {
-    int a = 1, b = 2;
-    // Массив из 2-х указателей на int
-    int *arrOfPtrs[2] = {&a, &b}; 
-    
-    int arr[3] = {10, 20, 30};
-    // Указатель на массив из 3-х int
-    int (*ptrToArr)[3] = &arr; 
-    
-    printf("arrOfPtrs[0]: %d\n", *arrOfPtrs[0]);
-    printf("ptrToArr[0][1]: %d\n", (*ptrToArr)[1]); // 20
+int main(int argc, char *argv[]) {
+    if (argc < 2) return 1;
+    FILE *file = fopen(argv[1], "r");
+    if (!file) return 1;
+    int ch;
+    while ((ch = fgetc(file)) != EOF) {
+        putchar(ch);
+    }
+    fclose(file);
     return 0;
 }
