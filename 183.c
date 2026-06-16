d451919a63ff209397ecb18d653d0375aacc5e86
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 07:33:16 +0700

    Update 183.c

--- 183.c
+++ 183.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+#include <math.h>
+
+typedef struct { double x, y; } Point;
+typedef struct { Point topLeft, bottomRight; } Rectangle;
+
+double calculate_area(Rectangle r) {
+    double width = fabs(r.bottomRight.x - r.topLeft.x);
+    double height = fabs(r.topLeft.y - r.bottomRight.y);
+    return width * height;
+}
 
 int main() {
-    // Массив указателей, где каждый указывает на строковый литерал (константу)
-    const char *days[] = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница"};
-    
-    for (int i = 0; i < 5; i++) {
-        printf("День %d: %s\n", i + 1, days[i]);
-    }
+    Rectangle rect = {{-2.0, 5.0}, {3.0, 1.0}};
+    printf("Площадь: %.2f\n", calculate_area(rect));
     return 0;
 }
