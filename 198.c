4609367c0e7d0075feb94e1ca3229e8e79e8fb63
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 07:41:38 +0700

    Update 198.c

--- 198.c
+++ 198.c
@@ -1,7 +1,15 @@
-void inOrder(struct TreeNode* root) {
-    if (root != NULL) {
-        inOrder(root->left);
-        printf("%d ", root->data);
-        inOrder(root->right);
-    }
+#include <stdio.h>
+
+#define ACTIVE_LOG_LEVEL 2 // 1: ERROR, 2: WARN, 3: INFO
+
+#define LOG(level, msg) do { \
+    if (level <= ACTIVE_LOG_LEVEL) { \
+        printf("[LEVEL %d]: %s\n", level, msg); \
+    } \
+} while(0)
+
+int main() {
+    LOG(1, "Критическая ошибка!"); // Выведется
+    LOG(3, "Обычная информация.");   // Не выведется
+    return 0;
 }
