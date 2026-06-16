16d7dfb79f5761aa3678ea107226de92fa0f3d6c
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 07:57:25 +0700

    Update 257.c

--- 257.c
+++ 257.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
-#include <setjmp.h>
-
-jmp_buf buffer;
-
-void calculate() {
-    printf("Выполняем вычисления...\n");
-    longjmp(buffer, 1); // Мгновенный "прыжок" обратно к setjmp
-    printf("Этот код никогда не выполнится.\n");
-}
 
 int main() {
-    if (setjmp(buffer) == 0) {
-        printf("Блок try\n");
-        calculate();
-    } else {
-        printf("Блок catch: поймано исключение!\n");
+    FILE *file = fopen("data.txt", "r");
+    if (!file) return 1;
+    char buffer[256];
+    while (fgets(buffer, sizeof(buffer), file)) {
+        printf("%s", buffer);
     }
+    fclose(file);
     return 0;
 }
