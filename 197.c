b69c709054314d3bb50a8291841baaceeb0a7045
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 07:41:15 +0700

    Update 197.c

--- 197.c
+++ 197.c
@@ -1,7 +1,14 @@
-void preOrder(struct TreeNode* root) {
-    if (root != NULL) {
-        printf("%d ", root->data);
-        preOrder(root->left);
-        preOrder(root->right);
-    }
+#include <stdio.h>
+
+#define SWAP_T(T, a, b) do { \
+    T tmp = (a); \
+    (a) = (b); \
+    (b) = tmp; \
+} while(0)
+
+int main() {
+    float a = 1.5f, b = 3.5f;
+    SWAP_T(float, a, b);
+    printf("a = %.1f, b = %.1f\n", a, b);
+    return 0;
 }
