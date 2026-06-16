0f74bbcc97e688aeddec92036af261ea55da32b9
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 08:05:23 +0700

    Update 266.c

--- 266.c
+++ 266.c
@@ -1,19 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-struct User { int id; char name[20]; };
-
-int compareUsers(const void *a, const void *b) {
-    return ((struct User*)a)->id - ((struct User*)b)->id;
-}
-
-int main() {
-    struct User users[] = {{1, "Alice"}, {2, "Bob"}, {5, "Charlie"}};
-    struct User key = {2, ""}; // Ищем по ID
-    
-    struct User *res = bsearch(&key, users, 3, sizeof(struct User), compareUsers);
-    
-    if (res != NULL) printf("Найден пользователь: %s\n", res->name);
-    return 0;
+void append_record(const char *filename, const char *record) {
+    FILE *file = fopen(filename, "a"); // "a" для добавления в конец
+    if (file) {
+        fprintf(file, "%s\n", record);
+        fclose(file);
+    }
 }
