94fe33e60b649afa945802c85bb2dbcb7ca4deda
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 07:25:49 +0700

    Update 146.c

--- 146.c
+++ 146.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 
-int binarySearch(int arr[], int left, int right, int x) {
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (arr[mid] == x) return mid;
-        if (arr[mid] < x) left = mid + 1;
-        else right = mid - 1;
-    }
-    return -1;
+size_t get_length(const char *str) {
+    size_t len = 0;
+    while (str[len]) len++;
+    return len;
 }
 
 int main() {
-    int arr[] = {2, 3, 4, 10, 40};
-    int index = binarySearch(arr, 0, 4, 10);
-    printf("Индекс элемента 10: %d\n", index);
+    printf("Длина: %zu\n", get_length("Test string"));
     return 0;
 }
