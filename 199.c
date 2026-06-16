c036d0b73a30529ec592e94b7854542e3799bc60
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 07:42:01 +0700

    Update 199.c

--- 199.c
+++ 199.c
@@ -1,7 +1,21 @@
-void postOrder(struct TreeNode* root) {
-    if (root != NULL) {
-        postOrder(root->left);
-        postOrder(root->right);
-        printf("%d ", root->data);
-    }
+#include <stdio.h>
+#include <stddef.h>
+
+#define CONTAINER_OF(ptr, type, member) \
+    ((type *)((char *)(ptr) - offsetof(type, member)))
+
+struct Node {
+    int data;
+    int key;
+};
+
+int main() {
+    struct Node n = {10, 42};
+    int *key_ptr = &n.key;
+    
+    // Получаем указатель на структуру Node через указатель на её поле key
+    struct Node *n_ptr = CONTAINER_OF(key_ptr, struct Node, key);
+    
+    printf("Данные структуры: %d\n", n_ptr->data);
+    return 0;
 }
