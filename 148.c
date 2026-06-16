cd6160212013e6e700b3fba550612c7b06966933
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 07:26:39 +0700

    Update 148.c

--- 148.c
+++ 148.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
-void selectionSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        int min_idx = i;
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[min_idx]) min_idx = j;
-        }
-        int temp = arr[min_idx];
-        arr[min_idx] = arr[i];
-        arr[i] = temp;
+void print_table(int (*func)(int), int start, int end) {
+    for (int x = start; x <= end; x++) {
+        printf("f(%d) = %d\n", x, func(x));
     }
 }
 
+int square(int x) { return x * x; }
+int double_val(int x) { return x * 2; }
+
 int main() {
-    int arr[] = {29, 10, 14, 37, 13};
-    selectionSort(arr, 5);
-    for (int i=0; i<5; i++) printf("%d ", arr[i]);
+    printf("Квадраты:\n");
+    print_table(square, 1, 3);
+    printf("Удвоение:\n");
+    print_table(double_val, 1, 3);
     return 0;
 }
