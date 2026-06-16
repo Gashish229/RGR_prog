789564d5831d6f349b266a198681b1231c47a793
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 07:22:35 +0700

    Update and rename 142.c to 142/142.h

--- 142.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-
-int main() {
-    const char *str = "Hello, world!";
-    char *ptr = strstr(str, "world");
-    if (ptr != NULL) printf("Найдено: %s\n", ptr);
-    return 0;
-}
---
+++ 142/142.h
@@ -0,0 +1,4 @@
+#ifndef MYFUNC_H
+#define MYFUNC_H
+void print_hello();
+#endif
