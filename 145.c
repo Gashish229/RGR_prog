f571bc112889ff0c8281e6ec9a95068c5a4cb79e
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 07:24:59 +0700

    Update 145.c

--- 145.c
+++ 145.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-void printReverse(const char *str) {
-    if (*str == '\0') return;
-    printReverse(str + 1);
-    putchar(*str);
+double get_average(int arr[], int size) {
+    if (size == 0) return 0.0;
+    int sum = 0;
+    for (int i = 0; i < size; i++) sum += arr[i];
+    return (double)sum / size;
 }
 
 int main() {
-    printReverse("Hello");
-    printf("\n");
+    int arr[] = {2, 4, 6, 8};
+    printf("Среднее: %.2f\n", get_average(arr, 4));
     return 0;
 }
