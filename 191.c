9230e28238ef1de8b41d91d6f50227c114d65bd8
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 07:38:50 +0700

    Update 191.c

--- 191.c
+++ 191.c
@@ -1,24 +1,9 @@
 #include <stdio.h>
 
-void swap(int *a, int *b) { int t = *a; *a = *b; *b = t; }
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
 
-int partition(int arr[], int low, int high) {
-    int pivot = arr[high];
-    int i = (low - 1);
-    for (int j = low; j <= high - 1; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            swap(&arr[i], &arr[j]);
-        }
-    }
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
-}
-
-void quickSort(int arr[], int low, int high) {
-    if (low < high) {
-        int pi = partition(arr, low, high);
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
+int main() {
+    int x = 10, y = 20;
+    printf("Максимум: %d\n", MAX(x, y));
+    return 0;
 }
