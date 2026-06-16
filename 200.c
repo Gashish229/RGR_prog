b3855da75a5c3c88e787fc1f46fc51a9524ddd9b
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 07:42:22 +0700

    Update 200.c

--- 200.c
+++ 200.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-#define PRINT_EXPR(expr) printf(#expr " = %d\n", expr)
+
+#define PRINT_VAR_NAME_AND_VALUE(x) printf("Переменная %s = %d\n", #x, x)
 
 int main() {
-    int a = 5, b = 10;
-    PRINT_EXPR(a + b); // Выведет: a + b = 15
+    int magicNumber = 99;
+    PRINT_VAR_NAME_AND_VALUE(magicNumber);
     return 0;
 }
