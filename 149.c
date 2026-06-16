fbcc2159bacafa97b6a9e1f3ecc1d0ab6175c05b
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 07:27:26 +0700

    Update 149.c

--- 149.c
+++ 149.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 
-int sumOfDigits(int n) {
-    int sum = 0;
-    while (n != 0) {
-        sum += n % 10;
-        n /= 10;
-    }
-    return sum;
-}
+double add(double a, double b) { return a + b; }
+double sub(double a, double b) { return a - b; }
+double mul(double a, double b) { return a * b; }
+double div_op(double a, double b) { return b != 0 ? a / b : 0; }
 
 int main() {
-    printf("Сумма цифр 1234: %d\n", sumOfDigits(1234)); // 10
+    double (*ops[4])(double, double) = {add, sub, mul, div_op};
+    int choice;
+    double a, b;
+    
+    printf("0:+, 1:-, 2:*, 3:/\nВыберите операцию: ");
+    scanf("%d", &choice);
+    printf("Введите два числа: ");
+    scanf("%lf %lf", &a, &b);
+    
+    if (choice >= 0 && choice <= 3) {
+        printf("Результат: %.2lf\n", ops[choice](a, b));
+    } else {
+        printf("Неверный выбор.\n");
+    }
     return 0;
 }
