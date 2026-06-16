731bd4645eb60493ec21717816cf690c5cdb0e2e
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 07:26:09 +0700

    Update 147.c

--- 147.c
+++ 147.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void bubbleSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
+int* create_array(int size) {
+    int *arr = (int*)malloc(size * sizeof(int));
+    if (arr) {
+        for (int i = 0; i < size; i++) arr[i] = i * 10;
     }
+    return arr;
 }
 
 int main() {
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};
-    bubbleSort(arr, 7);
-    for (int i=0; i<7; i++) printf("%d ", arr[i]);
+    int *my_arr = create_array(5);
+    for (int i = 0; i < 5; i++) printf("%d ", my_arr[i]);
+    free(my_arr);
     return 0;
 }
