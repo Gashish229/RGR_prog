68199d808847748ab9d2c3e49f75bb03c8e72a4a
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 07:37:52 +0700

    Update 189.c

--- 189.c
+++ 189.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
-#define LOG_POS() printf("Ошибка в файле %s на строке %d\n", __FILE__, __LINE__)
+#include <string.h>
+
+typedef struct {
+    char name[50];
+    int age;
+} Person;
+
+int compare_persons(const Person *p1, const Person *p2) {
+    int name_cmp = strcmp(p1->name, p2->name);
+    if (name_cmp != 0) return name_cmp;
+    return p1->age - p2->age;
+}
 
 int main() {
-    LOG_POS();
+    Person p1 = {"Иван", 30}, p2 = {"Иван", 25};
+    int res = compare_persons(&p1, &p2);
+    if (res == 0) printf("Люди одинаковые\n");
+    else if (res > 0) printf("p1 больше p2\n");
+    else printf("p1 меньше p2\n");
     return 0;
 }
