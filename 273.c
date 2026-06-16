3b86692589f2efc2d4f7d312ab832ec1327361c7
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 08:08:26 +0700

    Update 273.c

--- 273.c
+++ 273.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
-
-int main() {
-    int num = 15; // 1111
-    int bit_pos = 2;
+unsigned int swap_bits(unsigned int num, int p1, int p2) {
+    unsigned int bit1 = (num >> p1) & 1;
+    unsigned int bit2 = (num >> p2) & 1;
     
-    // Инвертируем маску (00000100 -> 11111011) и применяем побитовое И
-    num = num & ~(1 << bit_pos); 
-    printf("Результат: %d\n", num); // 1011 (11 в десятичной)
-    return 0;
+    if (bit1 != bit2) {
+        // Инвертируем оба бита
+        num ^= (1U << p1) | (1U << p2);
+    }
+    return num;
 }
