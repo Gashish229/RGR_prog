4d51416ca0aedc1ad2d6703ddfd76b6257cc5f3c
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 07:30:01 +0700

    Update 177.c

--- 177.c
+++ 177.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+struct Student {
+    char name[50];
+    int age;
+};
+
+void update_age(struct Student *s, int new_age) {
+    s->age = new_age;
+}
 
 int main() {
-    int rows = 3, cols = 4;
-    // Массив указателей на строки
-    int **mat = (int **)malloc(rows * sizeof(int *));
-    for (int i = 0; i < rows; i++) {
-        mat[i] = (int *)malloc(cols * sizeof(int)); // Выделение строк
-    }
-    
-    mat[1][2] = 42;
-    printf("Элемент [1][2]: %d\n", mat[1][2]);
-    return 0; // Освобождение в следующей задаче
+    struct Student st = {"Иван", 20};
+    update_age(&st, 21);
+    printf("Новый возраст: %d\n", st.age);
+    return 0;
 }
