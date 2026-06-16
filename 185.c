52feac104f67cc1881242841f64221d030fbeb82
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-06-16 07:34:03 +0700

    Update 185.c

--- 185.c
+++ 185.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
-void *myMemcpy(void *dest, const void *src, size_t n) {
-    char *d = (char *)dest;
-    const char *s = (const char *)src;
-    while (n--) *d++ = *s++;
-    return dest;
+typedef struct {
+    char name[50];
+    char position[50];
+    double salary;
+} Employee;
+
+void print_employee(const Employee *emp) {
+    printf("Сотрудник: %s, Должность: %s, Зарплата: %.2f\n", 
+           emp->name, emp->position, emp->salary);
 }
 
 int main() {
-    int src[] = {1, 2, 3};
-    int dest[3];
-    myMemcpy(dest, src, sizeof(src));
-    printf("%d %d %d\n", dest[0], dest[1], dest[2]);
+    Employee emp = {"Алексей", "Инженер", 120000.0};
+    print_employee(&emp);
     return 0;
 }
